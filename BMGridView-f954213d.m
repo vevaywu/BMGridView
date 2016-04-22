875aa43db096ac0902f3a9607d1345b194cd9922
ZeroSoft <wubo518@gmail.com>
2016-04-22 14:55:27 +0800

    Added files via upload
    modify datasource

---
+++ BMGridView-f954213d.m
@@ -0,0 +1,70 @@
+//
+//  BMGridView.m
+//  BMButton
+//
+//  Created by Bob on 16/4/20.
+//  Copyright © 2016年 Bob. All rights reserved.
+//
+
+#import "BMGridView.h"
+#import "BMButton.h"
+
+@interface BMGridView()
+
+@end
+
+@implementation BMGridView
+
+- (void)setDelegate:(id<GridClickDelegate>)delegate
+{
+    _delegate = delegate;
+}
+
+- (void)setDataSource:(id<GridViewDataSource>)dataSource
+{
+    _dataSource = dataSource;
+    [self initButton];
+}
+
+- (void)initButton
+{
+    for (int index = 0; index< [_dataSource numberOfPositionsInGridView:self]; index++) {
+        BMButton *button = [[BMButton alloc]initWithTitle:[_dataSource gridView:self titleNameInPosition:index]];
+        button.position = index;
+        [self addSubview:button];
+        button.adjustsButtonSizeToFitWidth = YES;
+        button.image = [UIImage imageNamed:[_dataSource gridView:self iconNameInPosition:index]];
+        [button addTarget:self action:@selector(buttonClickAction:)];
+    }
+}
+
+- (void)buttonClickAction:(BMButton*)sender
+{
+    if (_delegate && [_delegate respondsToSelector:@selector(gridClickAction:)]) {
+        [_delegate gridClickAction:sender];
+    }
+}
+
+- (void)drawRect:(CGRect)rect
+{
+    [super drawRect:rect];
+    
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSetLineCap(context,kCGLineCapSquare);
+    CGContextSetLineWidth(context,1);
+    CGContextSetRGBStrokeColor(context,239/255.0, 239/255.0, 244/255.0, 1.0);
+    
+    CGContextBeginPath(context);
+    
+    for (int i = 0; i< GridCol; i++) {
+        CGContextMoveToPoint(context, 0, i * (ButtonWidth));
+        CGContextAddLineToPoint(context,(screen_width) , i * (ButtonWidth));
+        
+        CGContextMoveToPoint(context, i * (ButtonWidth), 0);
+        CGContextAddLineToPoint(context,i * (ButtonWidth), (GridCol -1) * (ButtonWidth));
+    }
+    
+    CGContextStrokePath(context);
+}
+
+@end
---
+++ ThirdViewController-f954213d.m
@@ -0,0 +1,39 @@
+//
+//  ThirdViewController.m
+//  BMGridView
+//
+//  Created by Xiu on 16/4/22.
+//  Copyright © 2016年 Xiu. All rights reserved.
+//
+
+#import "ThirdViewController.h"
+
+@interface ThirdViewController ()
+
+@end
+
+@implementation ThirdViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor redColor];
+    NSLog(@"Class Name:%@",NSStringFromClass([self class]));
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
